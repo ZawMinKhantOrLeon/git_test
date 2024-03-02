34d0ddd959505193dfbd662d5881d9027e5decff
ZawMinKhantOrLeon <zawminkhantorleon@gmail.com>
2024-03-02 13:29:54 +0630

    Add . gitignore and make some changes

---
+++ README.md
@@ -0,0 +1,1 @@
+Hello My Name is ZawMinKhant . THis project is to test git for job ready java batch 5 class
---
+++ src/git_test/MainEntry.java
@@ -0,0 +1,11 @@
+package git_test;
+
+public class MainEntry {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Application Start");
+		System.out.println("Modified");
+	}	
+
+}
